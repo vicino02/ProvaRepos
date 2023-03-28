72d2ac33a228b9bf42300f6539d8b77cc6ec18a6
n.vicino <vicino@resolteam.com>
2023-03-28 18:24:37 +0200

    ultime modifiche

---
+++ FordAuto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Class1
+{
+
+	string id = "";
+
+
+	public Class1()
+	{
+
+		Console.WriteLine("la tua auto risulta creata");
+	
+			MessageBox.Show("id validato");
+			if (id=="n.vicino")
+			{
+				MessageBox.Show("id valido, accesso consentito");
+			}
+			else
+			{
+               MessageBox.Show("id non valido,riprova");
+            }
+		}
+
+
+	}
+}
